5b370c8a2aa7b520fc0a13786b8aaef4e930325f
pankourgiou <60981745+pankourgiou@users.noreply.github.com>
2023-11-25 04:08:06 +0200

    Update and rename United.java to Operations9.swift

--- United.java
+++
@@ -1,5 +0,0 @@
-public class United {
-  public static void main(String[] args) {
-    System.out.println("Come visit Greece for vacations it's a beautiful place, go visit Turkey for vacations they are equally beautiful. It's a win win situation. Let's run an all friends algorithm, united we can do more");
-  }
-}
